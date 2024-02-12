6dc89cbc24e52aed8d4136b5e539e54c61fa4649
MartialMAY <mayamoumartial@gmail.com>
2024-02-12 18:05:25 +0100

    phpmailer now

---
+++ server.js
@@ -0,0 +1,52 @@
+
+const express = require('express');
+const app = express();
+
+const nodemailer = require("nodemailer");
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static('public'));
+app.use(express.json())
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/public/index.html')
+})
+
+app.post('/', (req, res)=>{
+    console.log(req.body);
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+            user: 'lauthentiquemay@gmail.com',
+            pass: 'jqvo omta fbtu nrql',
+        },
+    })
+
+    const mailOptions= {
+        from: req.body.name,
+        to: 'lauthentiquemay@gmail.com',
+        subject: `Message de ${req.body.name}: test`,
+        text: req.body.message
+
+    }
+
+    transporter.sendMail(mailOptions, (error, info)=>{
+        if(error){
+            console.log(error);
+            res.send('error');
+        } else{
+            console.log('Email sent: ' + info.response);
+            res.send('success')
+        }
+    })
+})
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})

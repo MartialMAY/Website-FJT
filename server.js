cde4d59a981c5348353bc101237c5d3428c48a57
MartialMAY <mayamoumartial@gmail.com>
2024-02-12 12:33:50 +0100

    deployement

--- server.js
+++ server.js
@@ -58,13 +58,6 @@ app.post('/', (req, res)=>{
 })
 
 const PORT = process.env.PORT || 5000;
-
-if (process.env.NODE_ENV === 'production') {
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
-} else {
-    app.listen(8080, () => {
-        console.log('Development server running on port 8080');
-    });
-}
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});

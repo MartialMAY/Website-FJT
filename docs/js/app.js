a5be72bb2baa36876f9e06afd9243985f9554c8c
MartialMAY <mayamoumartial@gmail.com>
2024-02-12 13:27:02 +0100

    url

--- docs/js/app.js
+++ docs/js/app.js
@@ -13,7 +13,7 @@ contactForm.addEventListener('submit', (e)=>{
     }
 
     let xhr = new XMLHttpRequest();
-    fetch('https://lucky-rugelach-5ff49c.netlify.app', {
+    fetch('https://main--lucky-rugelach-5ff49c.netlify.app', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
